df9137aad73b1318a9a308b2e86c1b44ae3d6bd5
Paul-Larkin <realpaullarkin@gmail.com>
2019-05-06 16:39:38 +0100

    Added Static Methods Exercises Folder

---
+++ Static_Methods_Exercises/Reverse_String.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/*
+ * @author: Paul Larkin
+ * @date: 06/05/2019
+ * @name: Reverse_String
+ * 
+ * @description: From java exersises book. 
+ * Write a method that accepts a String 
+ * and reverses it. It should print the reversed String
+ * to the screen (one word per line).
+ */
+
+ public class Reverse_String {
+
+    public static void main (String[]args){
+
+      System.out.println("Please Enter a Sentence");
+
+      Scanner userInput = new Scanner (System.in);
+
+      String input = userInput.nextLine();
+
+      userInput.close();
+
+      reverseAndPrint(input);
+    }
+
+    private static void reverseAndPrint (String input){
+
+      String reversed = "";
+
+      for(int i = input.length()-1; i >= 0; i--){
+        reversed += input.charAt(i);
+      }
+
+      for(int i = 0; i < reversed.length(); i++){
+        if(reversed.charAt(i)==' '){
+          System.out.println("");
+        }
+        else{
+          System.out.print(reversed.charAt(i));
+        }
+      }
+    }
+ }
