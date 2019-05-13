890b17ffbac200a7de9ff9cae83475596f928ad2
Paul-Larkin <realpaullarkin@gmail.com>
2019-05-13 21:09:43 +0100

    Added Prime Number and Armstrong Number Exercise

---
+++ Programming_Exercises/Check_Is_ArmstrongNumber.java
@@ -0,0 +1,63 @@
+package algorithms;
+
+import java.util.Scanner;
+
+/*
+ * @author: Paul_Larkin
+ * @date: 13/05/2019
+ * @description: Check if the user
+ * input integer is that of an 
+ * Armstrong Number.
+ * 
+ * An Armstrong number is one the is
+ * equal to the sum of each individual
+ * number it contains cubed.
+ * 
+ * Example: 371 = (3^3)+(7^3)+(1^3)
+ * 
+ */
+
+public class Check_Is_ArmstrongNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner userInput = new Scanner (System.in);
+		
+		System.out.println("Please enter a number");
+		
+		int number = 0;
+				
+		if(userInput.hasNextInt()) {
+			number = userInput.nextInt();
+		}
+		else {
+			System.out.println("Next time enter a number");
+			System.exit(0);
+		}
+		
+		userInput.close();
+		
+		checkIsArmstrong(number);
+
+	}
+	
+	public static void checkIsArmstrong(int number) {
+
+		int result = 0;
+		int originalNumber = number;
+		
+		while (number!=0) {
+			
+			int modRemainder = number%10;
+			result += (modRemainder*modRemainder*modRemainder);
+			number /= 10;
+		}
+		
+		if(originalNumber == result) {
+			System.out.print("True: "+originalNumber+" is an Armstrong Number");
+		}else {
+			System.out.print("Fale: "+originalNumber+" is Not an Armstrong Number");
+		}
+	}
+
+}
---
+++ Programming_Exercises/Check_Is_Prime.java
@@ -0,0 +1,61 @@
+package algorithms;
+
+import java.util.Scanner;
+
+/*
+ * @author: Paul_Larkin
+ * @date: 13/05/2019
+ * @description: Check if the user
+ * input integer is that of an 
+ * Prime Number.
+ * 
+ * Prime number is one the is
+ * divisible only by 1 and itself
+ * 
+ */
+
+public class Check_Is_Prime {
+
+	
+	public static void main(String[] args) {
+		
+		int number = 0;
+		
+		Scanner userInput = new Scanner (System.in);
+		
+		System.out.println("Please enter a number");
+		
+		if(userInput.hasNextInt()) {
+			number = userInput.nextInt();
+		}
+		else {
+			System.out.println("Next time enter a number");
+			System.exit(0);
+		}
+		
+		userInput.close();
+		
+		checkIsPrime(number);
+	}
+	
+	public static void checkIsPrime(int number) {
+		
+		boolean isPrime = true;
+		
+		for(int i = 2; i < Math.sqrt(number); i++) {
+			
+			if(number%i == 0) {
+				isPrime = false;
+			}
+		}
+			
+		if(isPrime) {
+			System.out.println("True: "+number+" is an Prime Number");
+		}else {
+			System.out.println("False: "+number+" is NOT an Prime Number");
+		}
+		
+		
+	}
+
+}
